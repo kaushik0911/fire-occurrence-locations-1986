cb00facb1efba643708832182dffa3b47c8685d5
kaushik shamantha <kaushik.s@fidenz.com>
2021-01-06 18:30:07 +0530

    bug-fixes and changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ApplicationController
   def index
     @geo_data = Geodatum.all
     gon.rabl
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @geo_data }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "/home", to: "home#index"
+
+  defaults format: :json do
+    get 'api/v1/locations.json', to: "home#index"
+  end
 end
